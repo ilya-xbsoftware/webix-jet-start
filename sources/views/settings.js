e2c5a21283eb4befb9b5d5a4ba8efe893ad9c4b8
roadToTheFuture <63185603+roadToTheFuture@users.noreply.github.com>
2021-04-16 15:42:50 +0300

    [lesson1] feat: added function that change button label

--- sources/views/settings.js
+++ sources/views/settings.js
@@ -6,16 +6,27 @@ export default class SettingsView extends JetView{
 			padding:30,	rows:[
 				{
 					view: "segmented",
+					localId:"toggle",
 					inputWidth: 400,
 					height:50,
-					label:"eng",
+					label:"English",
 					options: [
 						{ id:"en", value:"English" },
 						{ id:"ru", value:"Russian"}
-					]
+					],
+					click: () => this.changeLabelValue()
 				},
 				{}
 			]
 		};
+	}
+	changeLabelValue(){
+		const btn = this.$$("toggle");
+		const btnId = btn.getValue();
+		const options = btn.getOption(btnId);
+		const btnValue = options.value;
+  
+		btn.define("label", `${btnValue}`);
+		btn.refresh();
 	}
 }
